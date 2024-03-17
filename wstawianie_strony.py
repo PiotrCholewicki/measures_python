76d4df70023c93f4ce5784b834975296c1d6a573
Piotr Cholewicki <piotr.cholewicki@gmail.com>
2024-03-17 17:59:18 +0100

    added function for circuits

--- wstawianie_strony.py
+++ wstawianie_strony.py
@@ -14,10 +14,11 @@ ws2.column_dimensions['B'].width = 41
 
 
 class Strona:
-    def __init__(self, gk, gl, ob): #ob - obwody, gk - gniazdka w kuchni, gl - gniazdka w lazience,
+    def __init__(self, gk, gl, ob_1, ob_3): #ob - obwody, gk - gniazdka w kuchni, gl - gniazdka w lazience,
         self.gk = gk
         self.gl = gl
-        self.ob = ob
+        self.ob_1 = ob_1
+        self.ob_3 = ob_3
 
 # Funkcja do kopiowania wierszy wraz z formatowaniem i dodawaniem podziału strony
 def copy_rows(source_sheet, target_sheet, start_row, end_row, offset, nr_klatki, nr_mieszkania):
@@ -37,17 +38,40 @@ def copy_rows(source_sheet, target_sheet, start_row, end_row, offset, nr_klatki,
     target_sheet.cell(row=2 + offset, column=8, value=nr_mieszkania)
     target_sheet.cell(row=7 + offset, column=10, value=nr_mieszkania)
 
+#funkcja uzupelniajaca strony na podstawie danych wprowadzonych przez użytkownika
 def uzupelnij_strony(strona, offset):
     target_sheet = ws2
     index = 0
-
+    index_ob = 0
     for i in range(strona.gk):
         target_sheet.cell(row=13 + offset + index, column=2, value='Gn pt 2P+Z,16A,250V kuchnia ')
         index+=1
     for i in range(strona.gl):
         target_sheet.cell(row=13 + offset + index, column=2, value='Gn nt hermet 2P+Z,16A,250V łazienka ')
         index+=1
+    for i in range(ob_1): #1-fazowe
+        target_sheet.cell(row=50 + offset + index_ob, column=2, value='Obwod 1-fazowy ')
+        index_ob += 1
+    for i in range(ob_3): #3-fazowe
+        target_sheet.cell(row=50 + offset + index_ob, column=2, value='Obwod 3-fazowy ')
+        index_ob += 1
+#funkcja która przyjmuje input typu "3+2", zamienia to na 3 obwody 1 fazowe, i 2 obwody 3-fazowe, 
+#zwraca tablice w ktorej tab[0] to 1-fazowe, tab[1] to 3 fazowe
 
+def podaj_obwody(wyrazenie):
+    tab = [0,0]
+    i = 0
+    for _ in wyrazenie:
+        if wyrazenie[i] == '+':
+            break
+        else:
+            i+=1
+    tab[0] = int(wyrazenie[0:i])
+    tab[1] = wyrazenie[i+1:]
+    if(tab[1] ==''):
+        tab[1] = 0
+    tab[1] = int(tab[1])
+    return tab
 
 i = 0
 nr_klatki = 0
@@ -62,11 +86,15 @@ while True:
         break
     nr_mieszkania = input("Podaj nr mieszkania ")
     copy_rows(ws1, ws2, 1, ws1.max_row, ws1.max_row * i, nr_klatki, nr_mieszkania)
-    ob = int(input("Podaj ilosc obwodow "))
+    ob = input("Podaj ilosc obwodow: ")
+    #korzystajac z funkcji podaj_obwody przepisywane są wartości obwodow 1 i 3 fazowych do dwoch osobnych zmiennych
+    tab = podaj_obwody(ob)
+    ob_1 = tab[0] 
+    ob_3 = tab[1]
     gk = int(input("Podaj ilosc gniazdek w kuchni "))
     gl = int(input("Podaj ilosc gniazdek w łazience "))
     
-    strona = Strona(gk, gl, ob)
+    strona = Strona(gk, gl, ob_1, ob_3)
     uzupelnij_strony(strona, ws1.max_row*i)
     # Kopiowanie scalonych komórek - kolejne strony
     for merged_cell_range in ws1.merged_cells.ranges:
